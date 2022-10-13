a2290b992810a32d66614f3a5b5f621d25cc71de
Chase Davis <chase.davis@gmail.com>
2022-10-13 11:24:10 -0500

    Deploy script

---
+++ bin/prd.deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get parent directory of this file
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+# Get to root directory, regardless of where this script is run from
+cd "$parent_path"
+cd ..
+
+cp conf/docker/docker-compose.prd.yml docker-compose.yml &&
+cp conf/docker/compose-run.prd.sh compose-run.sh &&
+
+# Run dev environment
+eb deploy
